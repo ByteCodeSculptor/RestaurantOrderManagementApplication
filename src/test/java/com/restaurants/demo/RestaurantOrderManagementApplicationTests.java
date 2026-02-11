59567735cd4fb51e64b391d9cfeaaf09076269c3
User <user@Practos-MacBook-Pro.local>
2026-02-11 22:11:19 +0530

    Initial commit

---
+++ src/test/java/com/restaurants/demo/RestaurantOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurants.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
