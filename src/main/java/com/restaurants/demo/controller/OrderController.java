0b415c0710b8d065a6255d84a454e5c007364198
Ebison Sojan <ebison.04@gmail.com>
2026-02-15 12:07:22 +0530

    feat: used apiResponse in order management controller

--- src/main/java/com/restaurants/demo/controller/OrderController.java
+++ src/main/java/com/restaurants/demo/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.restaurants.demo.dto.request.OrderRequest;
 import com.restaurants.demo.dto.response.DailyReportResponse;
 import com.restaurants.demo.dto.response.OrderResponse;
 import com.restaurants.demo.entity.Order;
+import com.restaurants.demo.exception.ApiResponse;
 import com.restaurants.demo.mapper.OrderMapper;
 import com.restaurants.demo.service.OrderService;
 import com.restaurants.demo.util.OrderStatus;
@@ -33,15 +34,24 @@ public class OrderController {
     // Controller to create order
     @PostMapping
     @PreAuthorize("hasRole('STAFF')")
-    public ResponseEntity<OrderResponse> createOrder (@Valid @RequestBody OrderRequest orderRequest) {
+    public ResponseEntity<?> createOrder (@Valid @RequestBody OrderRequest orderRequest) {
         Order order = orderService.createOrder(orderRequest);
+        OrderResponse orderResponse = orderMapper.toResponse(order);
+
+        ApiResponse<OrderResponse> apiResponse = ApiResponse.<OrderResponse>builder()
+                .status(HttpStatus.CREATED.value())
+                .message("Successfully created the order!")
+                .data(orderResponse)
+                .build();
+
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(orderMapper.toResponse(order));
+                .body(apiResponse);
     }
 
+    // dynamic filtering controller whose access can be given to both admin and staff
     @GetMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
-    public ResponseEntity<List<OrderResponse>> getOrders (@RequestParam(required = false) OrderStatus status,
+    public ResponseEntity<?> getOrders (@RequestParam(required = false) OrderStatus status,
                                                           @RequestParam(required = false) Integer tableNumber,
                                                           @RequestParam(required = false) LocalDate startDate,
                                                           @RequestParam(required = false) LocalDate endDate,
@@ -53,32 +63,64 @@ public class OrderController {
         for (Order order : orders) {
             orderResponses.add (orderMapper.toResponse(order));
         }
+
+        ApiResponse<List<OrderResponse>> apiResponse = ApiResponse.<List<OrderResponse>>builder()
+                .status(HttpStatus.OK.value())
+                .message("Successfully fetched all orders!")
+                .data(orderResponses)
+                .build();
+
         return ResponseEntity.status(HttpStatus.OK)
-                .body(orderResponses);
+                .body(apiResponse);
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
-    public ResponseEntity<OrderResponse> getOrderById (@PathVariable Long id) {
+    public ResponseEntity<?> getOrderById (@PathVariable Long id) {
         Order order = orderService.getOrderById(id);
+
+        OrderResponse orderResponse = orderMapper.toResponse(order);
+
+        ApiResponse<OrderResponse> apiResponse = ApiResponse.<OrderResponse>builder()
+                .status(HttpStatus.CREATED.value())
+                .message("Successfully fetched the order!")
+                .data(orderResponse)
+                .build();
+
         return ResponseEntity.status(HttpStatus.OK)
-                .body(orderMapper.toResponse((order)));
+                .body(apiResponse);
     }
 
     @PatchMapping("/{id}/status")
     @PreAuthorize("hasRole('STAFF')")
-    public ResponseEntity<OrderResponse> updateOrderStatus (@PathVariable Long id,
+    public ResponseEntity<?> updateOrderStatus (@PathVariable Long id,
                                                             @RequestParam OrderStatus orderStatus) {
         Order order = orderService.updateOrderStatus(id, orderStatus);
+
+        OrderResponse orderResponse = orderMapper.toResponse(order);
+
+        ApiResponse<OrderResponse> apiResponse = ApiResponse.<OrderResponse>builder()
+                .status(HttpStatus.CREATED.value())
+                .message("Successfully updated the order status!")
+                .data(orderResponse)
+                .build();
+
         return ResponseEntity.status(HttpStatus.OK)
-                .body(orderMapper.toResponse(order));
+                .body(apiResponse);
     }
 
     @GetMapping("/reports/daily")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<DailyReportResponse> getDailyReport () {
+    public ResponseEntity<?> getDailyReport () {
         DailyReportResponse dailyReportResponse = orderService.getDailyReport();
+
+        ApiResponse<DailyReportResponse> apiResponse = ApiResponse.<DailyReportResponse>builder()
+                .status(HttpStatus.CREATED.value())
+                .message("Successfully fetched today's report!")
+                .data(dailyReportResponse)
+                .build();
+
         return ResponseEntity.status(HttpStatus.OK)
-                .body(dailyReportResponse);
+                .body(apiResponse);
     }
 }
--- src/main/java/com/restaurants/demo/mapper/OrderMapper.java
+++ src/main/java/com/restaurants/demo/mapper/OrderMapper.java
@@ -8,39 +8,37 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class OrderMapper {
 
     public OrderResponse toResponse(Order order) {
-        OrderResponse orderResponse = new OrderResponse();
-        orderResponse.setId(order.getId());
-        orderResponse.setTableNumber(order.getTableNumber());
-        orderResponse.setStatus(order.getStatus());
-        orderResponse.setTotalAmount(order.getTotalAmount());
-        orderResponse.setCreatedAt(order.getCreatedAt());
 
         List<OrderItemResponse> items = new ArrayList<>();
-
         for (OrderItem item : order.getItems()) {
             items.add(toItemResponse(item));
         }
 
-        orderResponse.setItems(items);
-
-        return orderResponse;
+        return OrderResponse.builder()
+                .id(order.getId())
+                .tableNumber(order.getTableNumber())
+                .status(order.getStatus())
+                .totalAmount(order.getTotalAmount())
+                .createdAt(order.getCreatedAt())
+                .items(items)
+                .build();
     }
 
     private OrderItemResponse toItemResponse(OrderItem item) {
-        OrderItemResponse orderItemResponse = new OrderItemResponse();
-        orderItemResponse.setId(item.getId());
-        orderItemResponse.setOrderId(item.getOrder().getId());
-        orderItemResponse.setMenuItemId(item.getMenuItem().getId());
-        orderItemResponse.setMenuItemName(item.getMenuItemName());
-        orderItemResponse.setQuantity(item.getQuantity());
-        orderItemResponse.setPriceAtOrderTime(item.getPriceAtOrderTime());
-        orderItemResponse.setSubtotal(item.getSubtotal());
-        return orderItemResponse;
+        return OrderItemResponse.builder()
+                .id(item.getId())
+                .orderId(item.getOrder().getId())
+                .menuItemId(item.getMenuItem().getId())
+                .menuItemName(item.getMenuItemName())
+                .quantity(item.getQuantity())
+                .priceAtOrderTime(item.getPriceAtOrderTime())
+                .subtotal(item.getSubtotal())
+                .build();
     }
 }
+
