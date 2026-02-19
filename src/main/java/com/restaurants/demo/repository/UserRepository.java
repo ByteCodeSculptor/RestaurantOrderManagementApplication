93c8bad93bdfeec619e90b85e66249b956bf7f47
Uday Bisone <udaykumarbisone10@gmail.com>
2026-02-19 11:53:16 +0530

    added brute force security

--- src/main/java/com/restaurants/demo/repository/UserRepository.java
+++ src/main/java/com/restaurants/demo/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.restaurants.demo.repository;
 
 import com.restaurants.demo.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 
@@ -16,4 +20,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.accountNonLocked = true, u.failedAttempts = 0, u.lockoutTime = NULL WHERE u.accountNonLocked = false AND u.lockoutTime < :now")
+    void unlockExpiredAccounts(LocalDateTime now);
 }
